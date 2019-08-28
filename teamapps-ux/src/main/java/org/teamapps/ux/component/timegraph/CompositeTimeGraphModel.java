34a06c6955cb7f202e1c81bcdefb605a19541dd1
Yann Massard <yann.massard@teamapps.org>
2019-08-28 17:43:51 +0200

    TimeGraph: fixes and refactorings

---
+++ teamapps-ux/src/main/java/org/teamapps/ux/component/timegraph/CompositeTimeGraphModel.java
@@ -0,0 +1,69 @@
+package org.teamapps.ux.component.timegraph;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.teamapps.event.Event;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class CompositeTimeGraphModel implements TimeGraphModel {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(CompositeTimeGraphModel.class);
+
+	private Event<Void> onDataChanged = new Event<>();
+
+	private final List<TimeGraphModel> delegates;
+
+	public CompositeTimeGraphModel(TimeGraphModel... delegates) {
+		this.delegates = Arrays.asList(delegates);
+		this.delegates.forEach(delegate -> delegate.onDataChanged().addListener(onDataChanged::fire));
+	}
+
+	@Override
+	public Event<Void> onDataChanged() {
+		return onDataChanged;
+	}
+
+	@Override
+	public List<TimeGraphZoomLevel> getZoomLevels() {
+		return delegates.stream()
+				.flatMap(timeGraphModel -> timeGraphModel.getZoomLevels().stream())
+				.distinct()
+				.sorted(Comparator.comparing(TimeGraphZoomLevel::getApproximateMillisecondsPerDataPoint).reversed())
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public Map<String, LineChartDataPoints> getDataPoints(Collection<String> lineIds, TimeGraphZoomLevel zoomLevel, Interval neededIntervalX) {
+		Map<String, LineChartDataPoints> points = delegates.stream()
+				.map(delegate -> delegate.getDataPoints(lineIds, zoomLevel, neededIntervalX))
+				.reduce((Map<String, LineChartDataPoints> map1,
+				         Map<String, LineChartDataPoints> map2) -> {
+					HashMap<String, LineChartDataPoints> m = new HashMap<>();
+					m.putAll(map1);
+					m.putAll(map2);
+					return m;
+				}).orElseGet(HashMap::new);
+		int count = points.values().stream()
+				.mapToInt(LineChartDataPoints::size)
+				.sum();
+		if (count > 10_000) {
+			LOGGER.debug("Number of datapoints {}", count);
+		}
+		return points;
+	}
+
+	@Override
+	public Interval getDomainX(Collection<String> lineIds) {
+		return delegates.stream()
+				.map(delegate -> delegate.getDomainX(lineIds))
+				.reduce((Interval interval1, Interval interval2) -> new Interval(Math.min(interval1.getMin(), interval2.getMin()), Math.max(interval1.getMax(), interval2.getMax())))
+				.orElseGet(() -> new Interval(0, 1));
+	}
+}
