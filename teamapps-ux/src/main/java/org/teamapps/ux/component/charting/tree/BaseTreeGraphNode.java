79bd8a4773db83be1f0950b4f74f9da99b684045
Yann Massard <yann.massard@teamapps.org>
2019-10-10 15:28:47 +0200

    UiTreeGraph: preparations for side list items; more simplifications

---
+++ teamapps-ux/src/main/java/org/teamapps/ux/component/charting/tree/BaseTreeGraphNode.java
@@ -0,0 +1,146 @@
+package org.teamapps.ux.component.charting.tree;
+
+import org.teamapps.common.format.Color;
+import org.teamapps.ux.component.charting.common.GraphNodeIcon;
+import org.teamapps.ux.component.charting.common.GraphNodeImage;
+import org.teamapps.ux.component.template.Template;
+
+import java.util.UUID;
+
+public class BaseTreeGraphNode<RECORD> {
+	private final String id = UUID.randomUUID().toString();
+	private int width;
+	private int height;
+	private Color backgroundColor = new Color(255, 255, 255);
+	private Color borderColor = new Color(100, 100, 100);
+	private float borderWidth = 1;
+	private float borderRadius = 3;
+	private GraphNodeImage image;
+	private GraphNodeIcon icon;
+	private Template template;
+	private RECORD record;
+	private Color connectorLineColor = new Color(100, 100, 100);
+	private int connectorLineWidth;
+	private String dashArray;
+
+	protected String getId() {
+		return id;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public BaseTreeGraphNode<RECORD> setWidth(int width) {
+		this.width = width;
+		return this;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public BaseTreeGraphNode<RECORD> setHeight(int height) {
+		this.height = height;
+		return this;
+	}
+
+	public Color getBackgroundColor() {
+		return backgroundColor;
+	}
+
+	public BaseTreeGraphNode<RECORD> setBackgroundColor(Color backgroundColor) {
+		this.backgroundColor = backgroundColor;
+		return this;
+	}
+
+	public Color getBorderColor() {
+		return borderColor;
+	}
+
+	public BaseTreeGraphNode<RECORD> setBorderColor(Color borderColor) {
+		this.borderColor = borderColor;
+		return this;
+	}
+
+	public float getBorderWidth() {
+		return borderWidth;
+	}
+
+	public BaseTreeGraphNode<RECORD> setBorderWidth(float borderWidth) {
+		this.borderWidth = borderWidth;
+		return this;
+	}
+
+	public float getBorderRadius() {
+		return borderRadius;
+	}
+
+	public BaseTreeGraphNode<RECORD> setBorderRadius(float borderRadius) {
+		this.borderRadius = borderRadius;
+		return this;
+	}
+
+	public GraphNodeImage getImage() {
+		return image;
+	}
+
+	public BaseTreeGraphNode<RECORD> setImage(GraphNodeImage image) {
+		this.image = image;
+		return this;
+	}
+
+	public GraphNodeIcon getIcon() {
+		return icon;
+	}
+
+	public BaseTreeGraphNode<RECORD> setIcon(GraphNodeIcon icon) {
+		this.icon = icon;
+		return this;
+	}
+
+	public Template getTemplate() {
+		return template;
+	}
+
+	public BaseTreeGraphNode<RECORD> setTemplate(Template template) {
+		this.template = template;
+		return this;
+	}
+
+	public RECORD getRecord() {
+		return record;
+	}
+
+	public BaseTreeGraphNode<RECORD> setRecord(RECORD record) {
+		this.record = record;
+		return this;
+	}
+
+	public Color getConnectorLineColor() {
+		return connectorLineColor;
+	}
+
+	public BaseTreeGraphNode<RECORD> setConnectorLineColor(Color connectorLineColor) {
+		this.connectorLineColor = connectorLineColor;
+		return this;
+	}
+
+	public int getConnectorLineWidth() {
+		return connectorLineWidth;
+	}
+
+	public BaseTreeGraphNode<RECORD> setConnectorLineWidth(int connectorLineWidth) {
+		this.connectorLineWidth = connectorLineWidth;
+		return this;
+	}
+
+	public String getDashArray() {
+		return dashArray;
+	}
+
+	public BaseTreeGraphNode<RECORD> setDashArray(String dashArray) {
+		this.dashArray = dashArray;
+		return this;
+	}
+}
--- teamapps-ux/src/main/java/org/teamapps/ux/component/charting/tree/TreeGraphNode.java
+++ teamapps-ux/src/main/java/org/teamapps/ux/component/charting/tree/TreeGraphNode.java
@@ -1,36 +1,13 @@
 package org.teamapps.ux.component.charting.tree;
 
-import org.teamapps.common.format.Color;
-import org.teamapps.ux.component.charting.common.GraphNodeIcon;
-import org.teamapps.ux.component.charting.common.GraphNodeImage;
-import org.teamapps.ux.component.template.Template;
+import java.util.List;
 
-import java.util.UUID;
+public class TreeGraphNode<RECORD> extends BaseTreeGraphNode<RECORD> {
 
-public class TreeGraphNode<RECORD> {
-
-	private final String id = UUID.randomUUID().toString();
 	private TreeGraphNode<RECORD> parent;
-	private int width;
-	private int height;
-	private Color backgroundColor = new Color(255, 255, 255);
-	private Color borderColor = new Color(100, 100, 100);
-	private float borderWidth = 1;
-	private float borderRadius = 3;
-
-	private GraphNodeImage image;
-	private GraphNodeIcon icon;
-
-	private Template template;
-	private RECORD record;
-	private Color connectorLineColor = new Color(100, 100, 100);
-	private int connectorLineWidth;
-	private String dashArray;
 	private boolean expanded;
-
-	protected String getId() {
-		return id;
-	}
+	private boolean hasLazyChildren = false;
+	private List<BaseTreeGraphNode<RECORD>> sideListNodes;
 
 	public TreeGraphNode<RECORD> getParent() {
 		return parent;
@@ -41,129 +18,30 @@ public class TreeGraphNode<RECORD> {
 		return this;
 	}
 
-	public int getWidth() {
-		return width;
-	}
-
-	public TreeGraphNode<RECORD> setWidth(int width) {
-		this.width = width;
-		return this;
-	}
-
-	public int getHeight() {
-		return height;
-	}
-
-	public TreeGraphNode<RECORD> setHeight(int height) {
-		this.height = height;
-		return this;
-	}
-
-	public Color getBackgroundColor() {
-		return backgroundColor;
-	}
-
-	public TreeGraphNode<RECORD> setBackgroundColor(Color backgroundColor) {
-		this.backgroundColor = backgroundColor;
-		return this;
-	}
-
-	public Color getBorderColor() {
-		return borderColor;
-	}
-
-	public TreeGraphNode<RECORD> setBorderColor(Color borderColor) {
-		this.borderColor = borderColor;
-		return this;
-	}
-
-	public float getBorderWidth() {
-		return borderWidth;
-	}
-
-	public TreeGraphNode<RECORD> setBorderWidth(float borderWidth) {
-		this.borderWidth = borderWidth;
-		return this;
-	}
-
-	public float getBorderRadius() {
-		return borderRadius;
-	}
-
-	public TreeGraphNode<RECORD> setBorderRadius(float borderRadius) {
-		this.borderRadius = borderRadius;
-		return this;
-	}
-
-	public GraphNodeImage getImage() {
-		return image;
-	}
-
-	public TreeGraphNode<RECORD> setImage(GraphNodeImage image) {
-		this.image = image;
-		return this;
-	}
-
-	public GraphNodeIcon getIcon() {
-		return icon;
-	}
-
-	public TreeGraphNode<RECORD> setIcon(GraphNodeIcon icon) {
-		this.icon = icon;
-		return this;
-	}
-
-	public Template getTemplate() {
-		return template;
-	}
-
-	public TreeGraphNode<RECORD> setTemplate(Template template) {
-		this.template = template;
-		return this;
-	}
-
-	public RECORD getRecord() {
-		return record;
-	}
-
-	public TreeGraphNode<RECORD> setRecord(RECORD record) {
-		this.record = record;
-		return this;
-	}
-
-	public Color getConnectorLineColor() {
-		return connectorLineColor;
-	}
-
-	public TreeGraphNode<RECORD> setConnectorLineColor(Color connectorLineColor) {
-		this.connectorLineColor = connectorLineColor;
-		return this;
-	}
-
-	public int getConnectorLineWidth() {
-		return connectorLineWidth;
-	}
-
-	public TreeGraphNode<RECORD> setConnectorLineWidth(int connectorLineWidth) {
-		this.connectorLineWidth = connectorLineWidth;
-		return this;
-	}
-
-	public String getDashArray() {
-		return dashArray;
-	}
-
-	public TreeGraphNode<RECORD> setDashArray(String dashArray) {
-		this.dashArray = dashArray;
-		return this;
-	}
-
 	public boolean isExpanded() {
 		return expanded;
 	}
 
 	public TreeGraphNode<RECORD> setExpanded(boolean expanded) {
 		this.expanded = expanded;
+		return this;
+	}
+
+	public boolean isHasLazyChildren() {
+		return hasLazyChildren;
+	}
+
+	public TreeGraphNode<RECORD> setHasLazyChildren(boolean hasLazyChildren) {
+		this.hasLazyChildren = hasLazyChildren;
+		return this;
+	}
+
+	public List<BaseTreeGraphNode<RECORD>> getSideListNodes() {
+		return sideListNodes;
+	}
+
+	public TreeGraphNode<RECORD> setSideListNodes(List<BaseTreeGraphNode<RECORD>> sideListNodes) {
+		this.sideListNodes = sideListNodes;
 		return this;
 	}
 }
