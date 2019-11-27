850a93d6667027c03e2d6c2abe86292e8cfc5794
Yann Massard <yann.massard@teamapps.org>
2019-11-27 01:13:30 +0100

    DataBinding: static convenience methods

--- teamapps-ux/src/main/java/org/teamapps/databinding/TwoWayBindableValue.java
+++ teamapps-ux/src/main/java/org/teamapps/databinding/TwoWayBindableValue.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,11 +19,26 @@
  */
 package org.teamapps.databinding;
 
+import org.teamapps.event.Event;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 public interface TwoWayBindableValue<T> extends ObservableValue<T>, MutableValue<T> {
 
 	// ------- utility -------
 
 	default void bindTwoWays(TwoWayBindableValue<T> other) {
 		DataBindings.bindTwoWays(this, other);
+	}
+
+	// === static ===
+
+	public static <T> TwoWayBindableValue<T> create(Event<T> changeEvent, Supplier<T> supplier, Consumer<T> consumer) {
+		return DataBindings.createTwoWayBindable(changeEvent, supplier, consumer);
+	}
+
+	public static <T> TwoWayBindableValue<T> create(Event<T> changeEvent, Consumer<T> consumer) {
+		return DataBindings.createTwoWayBindable(changeEvent, consumer);
 	}
 }
