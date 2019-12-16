c06771e390dc8a5d4921e4c71946e7c326891171
Matthias Bernstein <matthias.bernstein@teamapps.org>
2019-12-16 14:12:38 +0100

    fixed list tree model

--- teamapps-ux/src/main/java/org/teamapps/ux/model/ListTreeModel.java
+++ teamapps-ux/src/main/java/org/teamapps/ux/model/ListTreeModel.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 public class ListTreeModel<RECORD> extends AbstractTreeModel<RECORD> {
 
 	private List<RECORD> records = new ArrayList<>();
-	private BiPredicate<RECORD, String> searchPredicate = (record, queryString) -> record.toString().toLowerCase().contains(queryString.toLowerCase());
+	private BiPredicate<RECORD, String> searchPredicate = (record, queryString) -> record.toString() != null && record.toString().toLowerCase().contains(queryString.toLowerCase());
 
 	public ListTreeModel(List<RECORD> records) {
 		this(records, null);
@@ -23,9 +23,13 @@ public class ListTreeModel<RECORD> extends AbstractTreeModel<RECORD> {
 
 	@Override
 	public List<RECORD> getRecords(String query) {
-		return records.stream()
-				.filter(r -> searchPredicate.test(r, query))
-				.collect(Collectors.toList());
+		if (query == null || query.isEmpty()) {
+			return records;
+		} else {
+			return records.stream()
+					.filter(r -> searchPredicate.test(r, query))
+					.collect(Collectors.toList());
+		}
 	}
 
 	public List<RECORD> getRecords() {
@@ -35,6 +39,7 @@ public class ListTreeModel<RECORD> extends AbstractTreeModel<RECORD> {
 	public void setRecords(List<RECORD> records) {
 		this.records.clear();
 		this.records.addAll(records);
+		onAllNodesChanged.fire();
 	}
 
 	public BiPredicate<RECORD, String> getSearchPredicate() {
@@ -44,5 +49,5 @@ public class ListTreeModel<RECORD> extends AbstractTreeModel<RECORD> {
 	public void setSearchPredicate(BiPredicate<RECORD, String> searchPredicate) {
 		this.searchPredicate = searchPredicate;
 	}
-	
+
 }
