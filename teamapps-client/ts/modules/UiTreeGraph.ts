f4ed521914188ecddee366c0b22947e35c89662f
Yann Massard <yann.massard@teamapps.org>
2020-02-19 10:39:24 +0100

    Update License headers

--- teamapps-client/ts/modules/UiTreeGraph.ts
+++ teamapps-client/ts/modules/UiTreeGraph.ts
@@ -2,7 +2,7 @@
  * ========================LICENSE_START=================================
  * TeamApps
  * ---
- * Copyright (C) 2014 - 2019 TeamApps.org
+ * Copyright (C) 2014 - 2020 TeamApps.org
  * ---
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,8 +15,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * =====
-
+ * =========================LICENSE_END==================================
  */
 
 import * as d3 from "d3";
