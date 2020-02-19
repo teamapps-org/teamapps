13d3cf09547578973f3fc17a66f09cb38aaae5c0
Yann Massard <yann.massard@teamapps.org>
2020-02-19 10:39:24 +0100

    Update License headers

--- teamapps-client/ts/modules/webrtc/mediasoup-v3/socket.io-promise.ts
+++ teamapps-client/ts/modules/webrtc/mediasoup-v3/socket.io-promise.ts
@@ -1,7 +1,8 @@
 /*-
+ * ========================LICENSE_START=================================
  * TeamApps
  * ---
- * Copyright (C) 2019 TeamApps.org
+ * Copyright (C) 2014 - 2020 TeamApps.org
  * ---
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,6 +15,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * =========================LICENSE_END==================================
  */
 
  exports.promise = function(socket: any) {
