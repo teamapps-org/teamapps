1171dda028a124160649cfc26e078b1d62b19e9c
Yann Massard <yann.massard@teamapps.org>
2020-02-22 13:58:47 +0100

    client house-keeping: remove obsolete dependencies, upgrade dependencies, fix resulting issues

--- teamapps-client/ts/modules/UiTestHarness.ts
+++ teamapps-client/ts/modules/UiTestHarness.ts
@@ -21,10 +21,6 @@ import {UiConfigurationConfig} from "../generated/UiConfigurationConfig";
 import {TemplateRegistry} from "./TemplateRegistry";
 import {UiComponentConfig} from "../generated/UiComponentConfig";
 import {TeamAppsUiContext} from "./TeamAppsUiContext";
-import {UiRadioGroup} from "./UiRadioGroup";
-import {UiFieldEditingMode} from "../generated/UiFieldEditingMode";
-import {UiSwitch} from "./UiSwitch";
-import {createUiColorConfig} from "../generated/UiColorConfig";
 import {UiPictureChooser} from "./formfield/file/UiPictureChooser";
 import {UiFieldGroup} from "./formfield/UiFieldGroup";
 import {UiTextField} from "./formfield/UiTextField";
@@ -51,39 +47,6 @@ export class UiTestHarness {
 
 		(window as any).c = component;
 		document.body.appendChild(component.getMainElement());
-	}
-
-	private createRadioGroup() {
-		let component = new UiRadioGroup({
-			id: "asdf",
-			radioButtons: [{
-				label: "AAA",
-				value: "aaa"
-			}, {
-				label: "BBB",
-				value: "bbb"
-			}, {
-				label: "CCC",
-				value: "ccc"
-			}],
-			editingMode: UiFieldEditingMode.EDITABLE_IF_FOCUSED
-		}, new TestTeamAppsUiContext());
-		component.onValueChanged.addListener(eventObject => console.log("Value changed: " + eventObject.value));
-		return component;
-	}
-
-	private createSwitch() {
-		let component = new UiSwitch({
-			id: "asdf",
-			value: false,
-			editingMode: UiFieldEditingMode.EDITABLE_IF_FOCUSED,
-			uncheckedTrackColor: createUiColorConfig(0, 255, 0),
-			checkedTrackColor: createUiColorConfig(0, 0, 255),
-			uncheckedButtonColor: createUiColorConfig(0, 0, 0),
-			checkedButtonColor: createUiColorConfig(255, 255, 255)
-		}, new TestTeamAppsUiContext());
-		component.onValueChanged.addListener(eventObject => console.log("Value changed: " + eventObject.value));
-		return component;
 	}
 
 	private createUiPictureChooser() {
