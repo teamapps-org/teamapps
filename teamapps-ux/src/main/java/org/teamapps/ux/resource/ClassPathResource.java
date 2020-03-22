c8879c43a5b2427f9d99c5d65d651535fbfab67f
Yann Massard <yann.massard@teamapps.org>
2020-03-22 21:08:32 +0100

    ClassPathResource: make ClassLoader specifiable; fix default loading

--- teamapps-ux/src/main/java/org/teamapps/ux/resource/ClassPathResource.java
+++ teamapps-ux/src/main/java/org/teamapps/ux/resource/ClassPathResource.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,16 +25,31 @@ public class ClassPathResource implements Resource {
 
 	private final String resourceName;
 	private final String name;
+	private final ClassLoader classLoader;
 	private long length = -1;
 
 	public ClassPathResource(String resourceName) {
+		this(resourceName, null);
+	}
+
+	public ClassPathResource(String resourceName, ClassLoader classLoader) {
 		this.resourceName = resourceName;
 		this.name = resourceName.contains("/") ? resourceName.substring(resourceName.lastIndexOf('/') + 1) : resourceName;
+		this.classLoader = classLoader;
 	}
 
 	@Override
 	public InputStream getInputStream() {
-		return ClassLoader.getSystemResourceAsStream(resourceName);
+		InputStream is;
+		if (classLoader != null) {
+			is = classLoader.getResourceAsStream(resourceName);
+		} else {
+			is = getClass().getResourceAsStream(resourceName);
+			if (is == null) {
+				is = ClassLoader.getSystemResourceAsStream(resourceName);
+			}
+		}
+		return is;
 	}
 
 	@Override
