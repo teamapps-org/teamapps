441fe741867fa3ea5c01907a45b6c636d817555d
Yann Massard <yann.massard@teamapps.org>
2020-05-22 11:50:26 +0200

    MediaSoupV3HttpClient

---
+++ teamapps-ux/src/test/java/org/teamapps/ux/component/webrtc/MediaSoupV3HttpClientTest.java
@@ -0,0 +1,21 @@
+package org.teamapps.ux.component.webrtc;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class MediaSoupV3HttpClientTest {
+
+	@Test
+	@Ignore
+	public void testStreaming() throws Exception {
+		// new MediaSoupV3HttpClient().startStreamingFile("https://abc.xyz/0", "LH_Secret1_", "syncTest.webm", "stream1")
+		// 		.get();
+		// new MediaSoupV3HttpClient().startStreamingUrl("https://abc.xyz/0", "LH_Secret1_", "https://abc.xyz/foo.webm", "stream4")
+		// 		.get();
+		// for (int i = 0; i < 5; i++) {
+		// 	new MediaSoupV3HttpClient().stopStreaming("https://abc.xyz/0", "LH_Secret1_", "stream" + i)
+		// 			.get();
+		// }
+	}
+
+}
