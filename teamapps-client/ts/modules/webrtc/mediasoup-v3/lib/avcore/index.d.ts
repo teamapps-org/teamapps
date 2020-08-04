32ec920a8d36a4e0ff9ea49b2a10f57ec49c9450
Yann Massard <yann.massard@teamapps.org>
2020-08-04 16:53:37 +0200

    update mediasoup client sources

--- teamapps-client/ts/modules/webrtc/mediasoup-v3/lib/avcore/index.d.ts
+++ teamapps-client/ts/modules/webrtc/mediasoup-v3/lib/avcore/index.d.ts
@@ -1,4 +1,4 @@
-export { ERROR, ACTION, API_OPERATION, EVENT, STAT, MIXER_PIPE_TYPE, NEXMO } from './constants';
+export { ERROR, ACTION, API_OPERATION, EVENT, STAT, MIXER_PIPE_TYPE, NEXMO, HLS, PATH, SOCKET_ONLY_ACTIONS, REST_ACTIONS, MIXER_RENDER_TYPE } from './constants';
 export { MediasoupSocketApi } from './mediasoup-socket-api';
 export { IMediasoupApi } from './i-mediasoup-api';
 export * from './client-interfaces';
