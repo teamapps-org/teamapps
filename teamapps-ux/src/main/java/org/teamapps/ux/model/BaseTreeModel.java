38aeebc462321d3599feb4a1d1e725de5a1d0d0d
Yann Massard <yann.massard@teamapps.org>
2020-08-28 10:51:06 +0200

    Make BaseTreeModel extend TreeNodeInfoExtractor (just for clarity)

--- teamapps-ux/src/main/java/org/teamapps/ux/model/BaseTreeModel.java
+++ teamapps-ux/src/main/java/org/teamapps/ux/model/BaseTreeModel.java
@@ -2,11 +2,12 @@ package org.teamapps.ux.model;
 
 import org.teamapps.ux.component.node.TreeNode;
 import org.teamapps.ux.component.tree.TreeNodeInfo;
+import org.teamapps.ux.component.tree.TreeNodeInfoExtractor;
 
 import java.util.Collections;
 import java.util.List;
 
-public interface BaseTreeModel<RECORD> {
+public interface BaseTreeModel<RECORD> extends TreeNodeInfoExtractor<RECORD> {
 
 	/**
 	 * Get the child records of a node. This is currently only used for lazy parent nodes but might be used
