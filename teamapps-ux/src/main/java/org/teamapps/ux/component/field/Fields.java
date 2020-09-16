bdf2a1666c85d09bfdef4aa1cfbef309e9b6eb05
Matthias Bernstein <matthias.bernstein@teamapps.org>
2020-09-16 16:54:31 +0200

    fields: validate as required convenience

--- teamapps-ux/src/main/java/org/teamapps/ux/component/field/Fields.java
+++ teamapps-ux/src/main/java/org/teamapps/ux/component/field/Fields.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,6 +19,9 @@
  */
 package org.teamapps.ux.component.field;
 
+import org.teamapps.ux.i18n.TeamAppsDictionary;
+import org.teamapps.ux.session.CurrentSessionContext;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -26,24 +29,37 @@ import java.util.stream.Stream;
 
 public final class Fields {
 
-	private Fields() {
-		// private
-	}
+    private Fields() {
+        // private
+    }
 
-	public static boolean validateAll(AbstractField<?>... fields) {
-		return validateAll(Arrays.stream(fields));
-	}
+    public static boolean validateAll(AbstractField<?>... fields) {
+        return validateAll(Arrays.stream(fields));
+    }
 
-	public static boolean validateAll(List<AbstractField<?>> fields) {
-		return validateAll(fields.stream());
-	}
+    public static boolean validateAll(List<AbstractField<?>> fields) {
+        return validateAll(fields.stream());
+    }
 
-	public static boolean validateAll(Stream<AbstractField<?>> stream) {
-		List<FieldMessage> errorMessage = stream
-				.flatMap(field -> field.validate().stream())
-				.filter(message -> message.getSeverity() == FieldMessage.Severity.ERROR)
-				.collect(Collectors.toList()); // to List because we want ALL validators to run!
-		return errorMessage.size() == 0;
-	}
+    public static boolean validateAll(Stream<AbstractField<?>> stream) {
+        List<FieldMessage> errorMessage = stream
+                .flatMap(field -> field.validate().stream())
+                .filter(message -> message.getSeverity() == FieldMessage.Severity.ERROR)
+                .collect(Collectors.toList()); // to List because we want ALL validators to run!
+        return errorMessage.size() == 0;
+    }
+
+    public static boolean validateAllAsRequired(AbstractField<?>... fields) {
+        FieldMessage errorMessage = new FieldMessage(FieldMessage.Severity.ERROR, CurrentSessionContext.get().getLocalized(TeamAppsDictionary.REQUIRED_FIELD.getKey()));
+        boolean isNotEmpty = true;
+        for (AbstractField<?> field : fields) {
+            if (field.isEmpty()) {
+                field.removeCustomFieldMessage(errorMessage);
+                field.addCustomFieldMessage(errorMessage);
+                isNotEmpty = false;
+            }
+        }
+        return isNotEmpty;
+    }
 
 }
