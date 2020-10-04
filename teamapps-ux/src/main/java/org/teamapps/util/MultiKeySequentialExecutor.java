ce7e1ee759ef45f47865b60fe2947c759bb6926c
Yann Massard <yann.massard@teamapps.org>
2020-10-04 12:19:09 +0200

    MultiKeySequentialExecutor: increase max thread count

--- teamapps-ux/src/main/java/org/teamapps/util/MultiKeySequentialExecutor.java
+++ teamapps-ux/src/main/java/org/teamapps/util/MultiKeySequentialExecutor.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,22 +28,40 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Supplier;
 
 public class MultiKeySequentialExecutor<K> {
 
-	private static Logger LOGGER = LoggerFactory.getLogger(MultiKeySequentialExecutor.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(MultiKeySequentialExecutor.class);
 
-	private Map<K, SequentialExecutor> sequentialExecutors = new ConcurrentHashMap<>(); // synchronized on submit method level
-	private ExecutorService pool;
+	private final AtomicReference<MinMaxAverageStats> delayStats = new AtomicReference<>(new MinMaxAverageStats());
+	private final AtomicReference<MinMaxAverageStats> executionTimeStats= new AtomicReference<>(new MinMaxAverageStats());
+
+	private final Map<K, SequentialExecutor> sequentialExecutors = new ConcurrentHashMap<>(); // synchronized on submit method level
+	private final ExecutorService pool;
 
 	public MultiKeySequentialExecutor(int nThreads) {
-		pool = Executors.newFixedThreadPool(nThreads);
+		this(Executors.newFixedThreadPool(nThreads));
 	}
 
 	public MultiKeySequentialExecutor(ExecutorService executorService) {
 		pool = executorService;
+
+		ScheduledExecutorService statsLogExecutorService = Executors.newSingleThreadScheduledExecutor();
+		statsLogExecutorService.scheduleAtFixedRate(() -> {
+			MinMaxAverageStats delayStats = this.delayStats.getAndSet(new MinMaxAverageStats());
+			MinMaxAverageStats executionTimeStats = this.executionTimeStats.getAndSet(new MinMaxAverageStats());
+			if (delayStats.max > 3000) {
+				LOGGER.warn("Delays critical: min: {}, max: {}, avg: {}, count: {}", delayStats.getMin(), delayStats.getMax(), delayStats.getAvg(), delayStats.getCount());
+			}
+			if (executionTimeStats.max > 1000) {
+				LOGGER.warn("Execution times critical: min: {}, max: {}, avg: {}, count: {}", executionTimeStats.getMin(), executionTimeStats.getMax(), executionTimeStats.getAvg(), executionTimeStats.getCount());
+			}
+		}, 1, 1, TimeUnit.SECONDS);
 	}
 
 	public CompletableFuture<Void> submit(K key, Runnable task) {
@@ -71,7 +89,7 @@ public class MultiKeySequentialExecutor<K> {
 
 	public class SequentialExecutor implements Executor {
 		private CompletableFuture<?> lastFuture = CompletableFuture.completedFuture(null);
-		private AtomicInteger queueSize = new AtomicInteger(0);
+		private final AtomicInteger queueSize = new AtomicInteger(0);
 		private boolean closed = false;
 
 		@Override
@@ -100,14 +118,10 @@ public class MultiKeySequentialExecutor<K> {
 			CompletableFuture<V> returnedFuture = lastFuture.thenApplyAsync(o -> {
 				long executionStartTime = System.currentTimeMillis();
 				long delay = executionStartTime - submitTime;
-				if (delay > 3000) {
-					LOGGER.warn("Execution delay high: {}", delay);
-				}
+				delayStats.getAndUpdate(minMaxAverageStats -> minMaxAverageStats.push(delay));
 				V result = task.get();
 				long executionTime = System.currentTimeMillis() - executionStartTime;
-				if (executionTime > 1000) {
-					LOGGER.warn("Execution time long: {}", executionTime);
-				}
+				executionTimeStats.getAndUpdate(minMaxAverageStats -> minMaxAverageStats.push(executionTime));
 				this.queueSize.decrementAndGet();
 				return result;
 			}, pool);
@@ -128,4 +142,44 @@ public class MultiKeySequentialExecutor<K> {
 	public static class SequentialExecutorClosedException extends RuntimeException {
 	}
 
+	private static class MinMaxAverageStats {
+		private final long min;
+		private final long max;
+		private final long total;
+		private final long count;
+
+		public MinMaxAverageStats() {
+			this.min = Long.MAX_VALUE;
+			this.max = 0;
+			this.total = 0;
+			this.count = 0;
+		}
+
+		public MinMaxAverageStats(long min, long max, long total, long count) {
+			this.min = min;
+			this.max = max;
+			this.total = total;
+			this.count = count;
+		}
+
+		public MinMaxAverageStats push(long time) {
+			return new MinMaxAverageStats(Math.min(time, min), Math.max(time, max), this.total + time, this.count + 1);
+		}
+
+		public long getMin() {
+			return min;
+		}
+
+		public long getMax() {
+			return max;
+		}
+
+		public long getAvg() {
+			return total / count;
+		}
+
+		public long getCount() {
+			return count;
+		}
+	}
 }
