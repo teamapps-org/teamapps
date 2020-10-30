c45f1a079e80459d92d1e6b5e3635422a45abdd5
Yann Massard <yann.massard@teamapps.org>
2020-10-30 11:33:48 +0100

    allow servlets with asyncSupported flag

--- teamapps-ux/src/main/java/org/teamapps/server/ServletRegistration.java
+++ teamapps-ux/src/main/java/org/teamapps/server/ServletRegistration.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,15 +27,24 @@ public class ServletRegistration {
 
 	private final Servlet servlet;
 	private final Collection<String> mappings;
+	private final boolean asyncSupported;
 
 	public ServletRegistration(Servlet servlet, String mapping) {
-		this.servlet = servlet;
-		this.mappings = Collections.singleton(mapping);
+		this(servlet, mapping, false);
+	}
+
+	public ServletRegistration(Servlet servlet, String mapping, boolean asyncSupported) {
+		this(servlet, Collections.singleton(mapping), asyncSupported);
 	}
 
 	public ServletRegistration(Servlet servlet, Collection<String> mappings) {
+		this(servlet, mappings, false);
+	}
+
+	public ServletRegistration(Servlet servlet, Collection<String> mappings, boolean asyncSupported) {
 		this.servlet = servlet;
 		this.mappings = mappings;
+		this.asyncSupported = asyncSupported;
 	}
 
 	public Servlet getServlet() {
@@ -44,5 +53,9 @@ public class ServletRegistration {
 
 	public Collection<String> getMappings() {
 		return mappings;
+	}
+
+	public boolean isAsyncSupported() {
+		return asyncSupported;
 	}
 }
