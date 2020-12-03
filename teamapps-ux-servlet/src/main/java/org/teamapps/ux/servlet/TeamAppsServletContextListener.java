26a60dfb35fbd1dc1906a4b5f11151ee5753e4b9
Yann Massard <yann.massard@teamapps.org>
2020-12-03 15:10:38 +0100

    make uploadDirectory configurable

--- teamapps-ux-servlet/src/main/java/org/teamapps/ux/servlet/TeamAppsServletContextListener.java
+++ teamapps-ux-servlet/src/main/java/org/teamapps/ux/servlet/TeamAppsServletContextListener.java
@@ -52,7 +52,7 @@ public class TeamAppsServletContextListener implements ServletContextListener {
 		downloadFilterRegistration.setAsyncSupported(true);
 		downloadFilterRegistration.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true, "*");
 
-		Dynamic uploadServletRegistration = context.addServlet("teamapps-upload-servlet", new UploadServlet(teamAppsCore.getUploadManager()::addUploadedFile));
+		Dynamic uploadServletRegistration = context.addServlet("teamapps-upload-servlet", new UploadServlet(teamAppsCore.getConfig().getUploadDirectory(), teamAppsCore.getUploadManager()::addUploadedFile));
 		uploadServletRegistration.addMapping("/upload/*");
 		uploadServletRegistration.setMultipartConfig(new MultipartConfigElement(null, -1L, -1L, 1000_000));
 
