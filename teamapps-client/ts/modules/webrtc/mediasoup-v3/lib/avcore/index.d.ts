c4b427a6cd3329c090eaf1f07a5ca590184c006f
Matthias Bernstein <matthias.bernstein@teamapps.org>
2021-01-13 20:51:09 +0100

    updated license headers

--- teamapps-client/ts/modules/webrtc/mediasoup-v3/lib/avcore/index.d.ts
+++ teamapps-client/ts/modules/webrtc/mediasoup-v3/lib/avcore/index.d.ts
@@ -1,3 +1,22 @@
+/*-
+ * ========================LICENSE_START=================================
+ * TeamApps
+ * ---
+ * Copyright (C) 2014 - 2021 TeamApps.org
+ * ---
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =========================LICENSE_END==================================
+ */
 export { ERROR, ACTION, API_OPERATION, EVENT, STAT, MIXER_PIPE_TYPE, NEXMO, HLS, PATH, SOCKET_ONLY_ACTIONS, REST_ACTIONS, MIXER_RENDER_TYPE } from './constants';
 export { MediasoupSocketApi } from './mediasoup-socket-api';
 export { IMediasoupApi } from './i-mediasoup-api';
