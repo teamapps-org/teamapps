7b8394df2ddb7620b9593222f814408df68f392f
Yann Massard <yann.massard@teamapps.org>
2021-01-26 14:22:22 +0100

    fix localization npe

--- teamapps-ux/src/main/java/org/teamapps/ux/session/SessionContext.java
+++ teamapps-ux/src/main/java/org/teamapps/ux/session/SessionContext.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -177,7 +177,7 @@ public class SessionContext {
 
 	public String getLocalized(String key, Object... parameters) {
 		String value = rankingTranslationProvider.getTranslation(key, Arrays.asList(getLocale(), Locale.ENGLISH, Locale.FRENCH, Locale.GERMAN));
-		if (parameters != null) {
+		if (value != null && parameters != null) {
 			return MessageFormat.format(value, parameters);
 		}
 		return value;
