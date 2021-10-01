65c2e46ab5b8e4c92c07abd1729e011d6b433918
Yann Massard <yamass@gmail.com>
2021-10-01 14:06:58 +0200

    convenience setters for aria-label and title

--- teamapps-ux/src/main/java/org/teamapps/ux/component/Component.java
+++ teamapps-ux/src/main/java/org/teamapps/ux/component/Component.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -79,6 +79,14 @@ public interface Component extends ClientObject {
 
 	default void setShadow(Shadow shadow) {
 		setCssStyle("box-shadow", shadow.toCssString());
+	}
+
+	default void setAriaLabel(String ariaLabel) {
+		setAttribute("aria-label", ariaLabel);
+	}
+
+	default void setHtmlTitle(String title) {
+		setAttribute("title", title);
 	}
 
 	// == static methods ==
