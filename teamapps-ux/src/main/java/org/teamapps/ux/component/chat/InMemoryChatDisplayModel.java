a201b567dea69aafa5eb17f34c74065a2f4e057c
Yann Massard <yamass@gmail.com>
2022-01-28 21:33:28 +0100

    ChatDisplay: fixes

--- teamapps-ux/src/main/java/org/teamapps/ux/component/chat/InMemoryChatDisplayModel.java
+++ teamapps-ux/src/main/java/org/teamapps/ux/component/chat/InMemoryChatDisplayModel.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -81,11 +81,11 @@ public class InMemoryChatDisplayModel extends AbstractChatDisplayModel {
 		}
 	}
 
-	public void addMessage(Resolvable userImage, String userNickname, String text) {
-		addMessage(userImage, userNickname, text, null, null, false);
+	public ChatMessage addMessage(Resolvable userImage, String userNickname, String text) {
+		return addMessage(userImage, userNickname, text, null, null, false);
 	}
 
-	public void addMessage(Resolvable userImage, String userNickname, String text, List<ChatPhoto> photos, List<ChatFile> files, boolean deleted) {
+	public ChatMessage addMessage(Resolvable userImage, String userNickname, String text, List<ChatPhoto> photos, List<ChatFile> files, boolean deleted) {
 		ChatMessageBatch chatMessageBatch = transformList(chatMessages -> {
 			boolean firstMessage = chatMessages.size() == 0;
 			SimpleChatMessage message = new SimpleChatMessage(chatMessageIdCounter.incrementAndGet(), userImage, userNickname, text, photos, files, deleted);
@@ -93,6 +93,7 @@ public class InMemoryChatDisplayModel extends AbstractChatDisplayModel {
 			return new ChatMessageBatch(Collections.singletonList(message), firstMessage);
 		});
 		this.onMessagesAdded.fire(chatMessageBatch);
+		return chatMessageBatch.getMessages().get(0);
 	}
 
 	public void replaceAllMessages(List<ChatMessage> messages) {
