49ba81ae373e0e6673513574bad2941031a5c262
Yann Massard <yamass@gmail.com>
2022-03-21 11:46:38 +0100

    fix exception handling for fast lane executions

--- teamapps-ux/src/main/java/org/teamapps/uisession/UiSession.java
+++ teamapps-ux/src/main/java/org/teamapps/uisession/UiSession.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -250,12 +250,7 @@ public class UiSession {
 		}
 		updateClientMessageId(clientMessageId);
 		reviveConnection();
-		failsafeInvokeSessionListeners(sl -> sl.onUiEvent(sessionId, event)
-				.exceptionally(e -> {
-					LOGGER.error("Exception while handling ui event", e);
-					close(UiSessionClosingReason.SERVER_SIDE_ERROR);
-					return null;
-				}));
+		failsafeInvokeSessionListeners(sl -> sl.onUiEvent(sessionId, event));
 	}
 
 	public void handleQuery(int clientMessageId, UiQuery query) {
@@ -272,10 +267,6 @@ public class UiSession {
 				result -> {
 					sendAsyncWithErrorHandler(new QUERY_RESULT(clientMessageId, result));
 					statistics.queryResultSentFor(query);
-				},
-				exception -> {
-					LOGGER.error("Exception while handling ui event", exception);
-					close(UiSessionClosingReason.SERVER_SIDE_ERROR);
 				}
 		));
 	}
