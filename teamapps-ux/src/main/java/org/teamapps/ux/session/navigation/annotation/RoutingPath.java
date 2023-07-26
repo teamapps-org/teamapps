e89b272c2b633405834fd1fa0268792f813cdad0
Yann Massard <yamass@gmail.com>
2023-07-26 10:42:28 +0200

    Routing: support multiple @RoutingPath annotations on methods; sort route handlers (shortest path template first)

--- teamapps-ux/src/main/java/org/teamapps/ux/session/navigation/annotation/RoutingPath.java
+++ teamapps-ux/src/main/java/org/teamapps/ux/session/navigation/annotation/RoutingPath.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,10 +21,7 @@ package org.teamapps.ux.session.navigation.annotation;
 
 import org.teamapps.ux.session.navigation.RouteHandler;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  * Marks a method as {@link RouteHandler} method.
@@ -45,6 +42,7 @@ import java.lang.annotation.Target;
  */
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
+@Repeatable(RoutingPaths.class)
 public @interface RoutingPath {
 
 	/**
