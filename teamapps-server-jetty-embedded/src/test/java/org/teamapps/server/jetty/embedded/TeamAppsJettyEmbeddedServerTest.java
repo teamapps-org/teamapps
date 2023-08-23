9f07722257483d4207c37e135574196b08f720a4
Yann Massard <yamass@gmail.com>
2023-08-23 13:05:28 +0200

    ToolButton: rename popoverText to title and make functional

--- teamapps-server-jetty-embedded/src/test/java/org/teamapps/server/jetty/embedded/TeamAppsJettyEmbeddedServerTest.java
+++ teamapps-server-jetty-embedded/src/test/java/org/teamapps/server/jetty/embedded/TeamAppsJettyEmbeddedServerTest.java
@@ -19,20 +19,13 @@
  */
 package org.teamapps.server.jetty.embedded;
 
-import org.teamapps.common.format.Color;
 import org.teamapps.icon.material.MaterialIcon;
-import org.teamapps.ux.component.field.TextField;
+import org.teamapps.ux.component.panel.Panel;
 import org.teamapps.ux.component.rootpanel.RootPanel;
-import org.teamapps.ux.component.table.ListTableModel;
-import org.teamapps.ux.component.table.Table;
-import org.teamapps.ux.component.template.BaseTemplate;
+import org.teamapps.ux.component.toolbutton.ToolButton;
 import org.teamapps.ux.session.SessionContext;
 
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class TeamAppsJettyEmbeddedServerTest {
 
@@ -42,46 +35,15 @@ public class TeamAppsJettyEmbeddedServerTest {
 		new TeamAppsJettyEmbeddedServer((SessionContext sessionContext) -> {
 			RootPanel rootPanel = sessionContext.addRootPanel();
 
-			Table<User> table = new Table<>();
-			ListTableModel<User> model = new ListTableModel<>(
-					IntStream.rangeClosed(1, 100)
-							.mapToObj(i -> new User(ALL_ICONS.get(i), "Heinz " + i, Color.fromRgbValue(ThreadLocalRandom.current().nextInt())))
-							.collect(Collectors.toList())
-			);
-			table.setModel(model);
+			Panel panel = new Panel();
+			ToolButton toolButton = new ToolButton(MaterialIcon.ALARM_ON, "asdf");
+			panel.addToolButton(toolButton);
 
-			table.addColumn("a", "A", new TextField())
-					.setValueExtractor(user1 -> {
-						String firstName = user1.getFirstName();
-						System.out.println("a: " + firstName);
-						return firstName;
-					})
-					.setDisplayTemplate(BaseTemplate.LIST_ITEM_SMALL_ICON_SINGLE_LINE)
-					.setDisplayPropertyProvider((user, propertyNames) -> Map.of("icon", user.getIcon(), "caption", user.getFirstName()))
-					.setValueInjector((user, firstName) -> {
-						System.out.println("Injecting from a: " + firstName);
-						user.setFirstName(firstName);
-					});
-			table.addColumn("b", "B", new TextField())
-					.setValueExtractor(user1 -> {
-						String firstName = user1.getFirstName();
-						System.out.println("b: " + firstName);
-						return firstName;
-					})
-					.setDisplayTemplate(BaseTemplate.LIST_ITEM_SMALL_ICON_SINGLE_LINE)
-					.setDisplayPropertyProvider((user, propertyNames) -> Map.of("icon", user.getIcon(), "caption", user.getFirstName()))
-					.setValueInjector((user, firstName) -> {
-						System.out.println("Injecting from b: " + firstName);
-						user.setFirstName(firstName);
-					});
+			toolButton.onClick.addListener((eventData, disposable) -> toolButton.setTitle("222"));
 
-			table.setEditable(true);
+			panel.addToolButton(new ToolButton(MaterialIcon.ALARM_ON));
 
-			table.onCellValueChanged.addListener((eventData) -> {
-				table.applyCellValuesToRecord(eventData.getRecord());
-			});
-
-			rootPanel.setContent(table);
+			rootPanel.setContent(panel);
 		}, 8082).start();
 	}
 
