09b9da09d2d1c81108dd38c220937b7d8f361b50
Matthias Bernstein <matthias.bernstein@teamapps.org>
2023-11-02 10:30:35 +0100

    updated release script (allow Java > 15)

--- release.sh
+++ release.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-mvn -P javadoc-jar,sources-jar,license-handling,release release:prepare release:perform && cd target/checkout && mvn nexus-staging:release
+export JDK_JAVA_OPTIONS='--add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED --add-opens java.base/java.text=ALL-UNNAMED --add-opens java.desktop/java.awt.font=ALL-UNNAMED'
+mvn -e -P javadoc-jar,sources-jar,license-handling,release release:prepare release:perform && cd target/checkout && mvn nexus-staging:release
