585e40bc64967d586d8cf7ce7edbbf00f5802d1b
Yann Massard <yamass@gmail.com>
2024-06-21 16:04:41 +0200

    simplify ClientRecordCache

--- teamapps-ux/src/main/java/org/teamapps/ux/cache/record/legacy/ClientRecordCache.java
+++ teamapps-ux/src/main/java/org/teamapps/ux/cache/record/legacy/ClientRecordCache.java
@@ -25,18 +25,13 @@ import org.teamapps.dto.UiIdentifiableClientRecord;
 import org.teamapps.util.StreamUtil;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.BiPredicate;
-import java.util.function.Function;
 import java.util.stream.Collectors;
-
-import static java.util.function.Function.identity;
 
 public class ClientRecordCache<RECORD, UIRECORD extends UiIdentifiableClientRecord> {
 
@@ -45,8 +40,6 @@ public class ClientRecordCache<RECORD, UIRECORD extends UiIdentifiableClientReco
 	private final UiIdentifiableClientRecordFactory<RECORD, UIRECORD> clientRecordFactory;
 	private final UiIdentifiableClientRecordPostProcessor<RECORD, UIRECORD> postProcessor;
 	private int idCounter;
-	private int nextOperationSequenceNumber = 0;
-	private int operationInvalidationSequenceNumber = -1;
 
 	private ClientRecordCachePurgeListener purgeListener;
 	private BiPredicate<RECORD, Integer> purgeDecider = (record, clientRecordId) -> true;
@@ -55,19 +48,19 @@ public class ClientRecordCache<RECORD, UIRECORD extends UiIdentifiableClientReco
 	/**
 	 * Always represents the current server-side state.
 	 */
-	private LinkedHashMap<RECORD, Integer> uiRecordsByRecord = new LinkedHashMap<>();
+	private final LinkedHashMap<RECORD, Integer> uiRecordsByRecord = new LinkedHashMap<>();
 
 	/**
 	 * Always represents the currently acknowledged client-side state.
 	 */
-	private LinkedHashMap<Integer, RECORD> recordsByClientId = new LinkedHashMap<>();
+	private final LinkedHashMap<Integer, RECORD> recordsByClientId = new LinkedHashMap<>();
 
 	/**
 	 * Represents records not yet acknowledged by the client side.
 	 * Example: Setting records of a tree, the tree might request child components of an expanded lazy node during its initialization. The corresponding "lazy children" request will come
 	 * before the actual acknowledgement of the tree data (with the parent lazy expanded node).
 	 */
-	private LinkedHashMap<Integer, RECORD> unacknowledgedRecordsByClientId = new LinkedHashMap<>();
+	private final LinkedHashMap<Integer, RECORD> unacknowledgedRecordsByClientId = new LinkedHashMap<>();
 
 	public ClientRecordCache(UiIdentifiableClientRecordFactory<RECORD, UIRECORD> clientRecordFactory, UiIdentifiableClientRecordPostProcessor<RECORD, UIRECORD> postProcessor) {
 		this.clientRecordFactory = clientRecordFactory;
@@ -86,13 +79,6 @@ public class ClientRecordCache<RECORD, UIRECORD extends UiIdentifiableClientReco
 		return uiRecordsByRecord.get(record);
 	}
 
-	public List<Integer> getUiRecordIds(List<RECORD> records) {
-		return records.stream()
-				.map(record -> getUiRecordIdOrNull(record))
-				.filter(clientId -> clientId != null)
-				.collect(Collectors.toList());
-	}
-
 	public RECORD getRecordByClientId(int id) {
 		RECORD record = recordsByClientId.get(id);
 		if (record == null) {
@@ -106,7 +92,7 @@ public class ClientRecordCache<RECORD, UIRECORD extends UiIdentifiableClientReco
 
 	public CacheManipulationHandle<Void> clear() {
 		uiRecordsByRecord.clear();
-		return new CacheManipulationHandle<>(this, nextOperationSequenceNumber++, null, () -> recordsByClientId.clear());
+		return new CacheManipulationHandle<>(null, recordsByClientId::clear);
 	}
 
 	/**
@@ -119,13 +105,13 @@ public class ClientRecordCache<RECORD, UIRECORD extends UiIdentifiableClientReco
 
 		this.uiRecordsByRecord.clear();
 		this.uiRecordsByRecord.putAll(uiRecordsByRecord.entrySet().stream()
-				.collect(Collectors.toMap(entry -> entry.getKey(), entry -> entry.getValue().getId())));
+				.collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().getId())));
 
 		Map<Integer, RECORD> newRecordsByClientId = uiRecordsByRecord.entrySet().stream()
 				.collect(Collectors.toMap(entry -> entry.getValue().getId(), Map.Entry::getKey));
 		this.unacknowledgedRecordsByClientId.putAll(newRecordsByClientId);
 
-		return new CacheManipulationHandle<>(this, nextOperationSequenceNumber++, new ArrayList<>(uiRecordsByRecord.values()),
+		return new CacheManipulationHandle<>(new ArrayList<>(uiRecordsByRecord.values()),
 				() -> {
 					this.recordsByClientId.putAll(newRecordsByClientId);
 					this.unacknowledgedRecordsByClientId.keySet().removeAll(newRecordsByClientId.keySet());
@@ -136,13 +122,13 @@ public class ClientRecordCache<RECORD, UIRECORD extends UiIdentifiableClientReco
 		purgeIfNeeded(newRecords.size());
 
 		LinkedHashMap<RECORD, UIRECORD> uiRecordsByRecord = createUiRecords(newRecords);
-		this.uiRecordsByRecord.putAll(uiRecordsByRecord.entrySet().stream().collect(StreamUtil.toLinkedHashMap(entry -> entry.getKey(), entry -> entry.getValue().getId())));
+		this.uiRecordsByRecord.putAll(uiRecordsByRecord.entrySet().stream().collect(StreamUtil.toLinkedHashMap(Map.Entry::getKey, entry -> entry.getValue().getId())));
 
 		Map<Integer, RECORD> newRecordsByClientId = uiRecordsByRecord.entrySet().stream()
 				.collect(Collectors.toMap(entry -> entry.getValue().getId(), Map.Entry::getKey));
 		this.unacknowledgedRecordsByClientId.putAll(newRecordsByClientId);
 
-		return new CacheManipulationHandle<>(this, nextOperationSequenceNumber++, new ArrayList<>(uiRecordsByRecord.values()),
+		return new CacheManipulationHandle<>(new ArrayList<>(uiRecordsByRecord.values()),
 				() -> {
 					recordsByClientId.putAll(newRecordsByClientId);
 					this.unacknowledgedRecordsByClientId.keySet().removeAll(newRecordsByClientId.keySet());
@@ -157,55 +143,15 @@ public class ClientRecordCache<RECORD, UIRECORD extends UiIdentifiableClientReco
 		uiRecordsByRecord.put(record, uiRecord.getId());
 		this.unacknowledgedRecordsByClientId.put(uiRecord.getId(), record);
 
-		return new CacheManipulationHandle<>(this, nextOperationSequenceNumber++, uiRecord, () -> {
+		return new CacheManipulationHandle<>(uiRecord, () -> {
 			recordsByClientId.put(uiRecord.getId(), record);
 			unacknowledgedRecordsByClientId.remove(uiRecord.getId());
 		});
 	}
 
-	public CacheManipulationHandle<UIRECORD> addOrUpdateRecord(RECORD record) {
-		Integer oldUiRecord = getUiRecordIdOrNull(record);
-		UIRECORD newUiRecord = createUiRecord(record);
-		postProcessor.postProcess(record, newUiRecord, Collections.singletonMap(record, newUiRecord));
-		if (oldUiRecord != null) {
-			newUiRecord.setId(oldUiRecord);
-		} else {
-			purgeIfNeeded(1);
-		}
-
-		uiRecordsByRecord.put(record, newUiRecord.getId());
-		this.unacknowledgedRecordsByClientId.put(newUiRecord.getId(), record);
-
-		return new CacheManipulationHandle<>(this, nextOperationSequenceNumber++, newUiRecord, () -> {
-			recordsByClientId.put(newUiRecord.getId(), record);
-			unacknowledgedRecordsByClientId.remove(newUiRecord.getId());
-		});
-	}
-
 	public CacheManipulationHandle<Integer> removeRecord(RECORD record) {
 		Integer uiRecord = uiRecordsByRecord.remove(record);
-		return new CacheManipulationHandle<>(this, nextOperationSequenceNumber++, uiRecord, () -> recordsByClientId.remove(uiRecord));
-	}
-
-	public CacheManipulationHandle<List<Integer>> removeRecords(Collection<RECORD> recordToBeRemoved) {
-		List<Integer> removedUiIds = new ArrayList<>();
-		uiRecordsByRecord.entrySet().removeIf(entry -> {
-			boolean toBeRemoved = recordToBeRemoved.contains(entry.getKey());
-			if (toBeRemoved) {
-				removedUiIds.add(entry.getValue());
-			}
-			return toBeRemoved;
-		});
-		return new CacheManipulationHandle<>(this, nextOperationSequenceNumber++, removedUiIds, () -> recordsByClientId.entrySet().removeIf(entry -> removedUiIds.contains(entry.getKey())));
-	}
-
-	public void hardReplaceRecords(Map<RECORD, Integer> newRecords) {
-		this.uiRecordsByRecord.clear();
-		this.uiRecordsByRecord.putAll(newRecords);
-		this.recordsByClientId.clear();
-		this.recordsByClientId.putAll(newRecords.entrySet().stream().collect(Collectors.toMap(entry -> entry.getValue(), entry -> entry.getKey())));
-		this.unacknowledgedRecordsByClientId.clear();
-		this.operationInvalidationSequenceNumber = nextOperationSequenceNumber - 1;
+		return new CacheManipulationHandle<>(uiRecord, () -> recordsByClientId.remove(uiRecord));
 	}
 
 	private void purgeIfNeeded(int numberOfRecordsToBeAdded) {
@@ -214,9 +160,7 @@ public class ClientRecordCache<RECORD, UIRECORD extends UiIdentifiableClientReco
 			List<Integer> removedClientRecordIds = removeEldestEntries(uiRecordsByRecord, numberOfRecordsToBeRemoved, purgeDecider);
 
 			if (purgeListener != null) {
-				purgeListener.handleCacheEntriesPurged(new CacheManipulationHandle<>(this, nextOperationSequenceNumber++, removedClientRecordIds, () -> {
-					recordsByClientId.keySet().removeAll(removedClientRecordIds);
-				}));
+				purgeListener.handleCacheEntriesPurged(new CacheManipulationHandle<>(removedClientRecordIds, () -> recordsByClientId.keySet().removeAll(removedClientRecordIds)));
 			}
 		}
 
@@ -241,22 +185,14 @@ public class ClientRecordCache<RECORD, UIRECORD extends UiIdentifiableClientReco
 
 	private LinkedHashMap<RECORD, UIRECORD> createUiRecords(List<RECORD> newRecords) {
 		LinkedHashMap<RECORD, UIRECORD> uiRecordsByRecord = newRecords.stream()
-				.collect(StreamUtil.toLinkedHashMap(record -> record, record -> createUiRecord(record)));
+				.collect(StreamUtil.toLinkedHashMap(record -> record, this::createUiRecord));
 		uiRecordsByRecord.forEach((record, uiRecord) -> postProcessor.postProcess(record, uiRecord, uiRecordsByRecord));
 		return uiRecordsByRecord;
-	}
-
-	public int getMaxCapacity() {
-		return maxCapacity;
 	}
 
 	public void setMaxCapacity(int maxCapacity) {
 		this.maxCapacity = maxCapacity;
 		purgeIfNeeded(0);
-	}
-
-	public ClientRecordCachePurgeListener getPurgeListener() {
-		return purgeListener;
 	}
 
 	public void setPurgeListener(ClientRecordCachePurgeListener purgeListener) {
@@ -269,44 +205,12 @@ public class ClientRecordCache<RECORD, UIRECORD extends UiIdentifiableClientReco
 		return uiRecord;
 	}
 
-	public int getOperationInvalidationSequenceNumber() {
-		return operationInvalidationSequenceNumber;
-	}
-
-	public Map<Integer, RECORD> getAllRecords(boolean includingAcknowledged) {
-		var result =  new HashMap<>(recordsByClientId);
-		if (includingAcknowledged) {
-			result.putAll(unacknowledgedRecordsByClientId);
-		}
-		return result;
-	}
-
-	public <ID> boolean containsExactly(List<RECORD> records, Function<RECORD, ID> identifierExtractor, BiPredicate<RECORD, RECORD> recordsEqual) {
-		if (uiRecordsByRecord.size() != records.size()) {
-			return false;
-		}
-		Map<ID, RECORD> cachedRecordsById = uiRecordsByRecord.keySet().stream()
-				.collect(Collectors.toMap(identifierExtractor, identity()));
-		for (RECORD record : records) {
-			ID recordId = identifierExtractor.apply(record);
-			RECORD cachedRecord = cachedRecordsById.get(recordId);
-			if (cachedRecord == null || !recordsEqual.test(record, cachedRecord)) {
-				return false;
-			}
-		}
-		return true;
-	}
-
 	public interface UiIdentifiableClientRecordFactory<RECORD, UIRECORD> {
 		UIRECORD create(RECORD record);
 	}
 
 	public interface UiIdentifiableClientRecordPostProcessor<RECORD, UIRECORD> {
 		void postProcess(RECORD record, UIRECORD uiRecord, Map<RECORD, UIRECORD> allNewUiRecords);
-	}
-
-	public BiPredicate<RECORD, Integer> getPurgeDecider() {
-		return purgeDecider;
 	}
 
 	public void setPurgeDecider(BiPredicate<RECORD, Integer> purgeDecider) {
