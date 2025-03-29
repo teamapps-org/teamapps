ab1a151426b3b9576fdac39d40194ab9eac92c7e
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-03-29 23:06:36 +0100

    UiPdfViewer: add PdfViewer Test environment in TeamAppsJettyEmbeddedServerTest.java

--- teamapps-server-jetty-embedded/src/test/java/org/teamapps/server/jetty/embedded/TeamAppsJettyEmbeddedServerTest.java
+++ teamapps-server-jetty-embedded/src/test/java/org/teamapps/server/jetty/embedded/TeamAppsJettyEmbeddedServerTest.java
@@ -21,9 +21,11 @@ package org.teamapps.server.jetty.embedded;
 
 import org.teamapps.common.format.RgbaColor;
 import org.teamapps.ux.component.field.TextField;
+import org.teamapps.ux.component.pdfviewer.PdfViewer;
 import org.teamapps.ux.component.rootpanel.RootPanel;
 import org.teamapps.ux.component.table.AbstractTableModel;
 import org.teamapps.ux.component.table.Table;
+import org.teamapps.ux.resource.ClassPathResource;
 import org.teamapps.webcontroller.WebController;
 
 import java.util.*;
@@ -32,60 +34,14 @@ import java.util.stream.IntStream;
 
 public class TeamAppsJettyEmbeddedServerTest {
 
-	public static final List<RgbaColor> FOREGROUND_COLORS = Arrays.asList(
-			RgbaColor.MATERIAL_GREEN_500,
-			RgbaColor.MATERIAL_RED_700,
-			RgbaColor.MATERIAL_BLUE_600,
-			RgbaColor.MATERIAL_YELLOW_600,
-			RgbaColor.MATERIAL_PURPLE_500,
-			RgbaColor.MATERIAL_BROWN_500,
-			RgbaColor.MATERIAL_PINK_500,
-			RgbaColor.MATERIAL_DEEP_ORANGE_500
-	);
-
-	public static <T> T randomOf(Collection<T> collection) {
-		List<T> list = new ArrayList<>(collection);
-		return list.get(ThreadLocalRandom.current().nextInt(list.size()));
-	}
-
-	public static RgbaColor randomColor() {
-		return randomOf(FOREGROUND_COLORS);
-	}
-
 	public static void main(String[] args) throws Exception {
 		WebController controller = sessionContext -> {
 			RootPanel rootPanel = new RootPanel();
 			sessionContext.addRootPanel(null, rootPanel);
 
-			Table<String> table = new Table<>();
-			table.addColumn("a", "a", new TextField()).setValueExtractor(Objects::toString);
-			table.addColumn("b", "b", new TextField()).setValueExtractor(Objects::toString);
-			table.addColumn("c", "c", new TextField()).setValueExtractor(Objects::toString);
-			table.addColumn("d", "d", new TextField()).setValueExtractor(Objects::toString);
-			table.addColumn("e", "e", new TextField()).setValueExtractor(Objects::toString);
-			table.addColumn("f", "f", new TextField()).setValueExtractor(Objects::toString);
-			table.addColumn("g", "g", new TextField()).setValueExtractor(Objects::toString);
-			table.addColumn("h", "h", new TextField()).setValueExtractor(Objects::toString);
-
-			table.setRowCssStyleProvider(s -> Map.of("background-color", randomColor().toHtmlColorString()));
-
-			table.setModel(new AbstractTableModel<String>() {
-
-				private final List<String> data = IntStream.range(0, 10000).mapToObj(value -> "" + value).toList();
-
-				@Override
-				public int getCount() {
-					return data.size();
-				}
-
-				@Override
-				public List<String> getRecords(int startIndex, int length) {
-					return data.stream().skip(startIndex).limit(length).toList();
-				}
-			});
-			table.setCellMarked("12", "x", true);
-
-			rootPanel.setContent(table);
+			String testPdfLink = sessionContext.createResourceLink(new ClassPathResource("test.pdf", "application/pdf" ));
+			PdfViewer component = new PdfViewer(testPdfLink);
+			rootPanel.setContent(component);
 		};
 
 		TeamAppsJettyEmbeddedServer.builder(controller)
