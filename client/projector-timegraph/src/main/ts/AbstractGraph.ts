d50631cb454e2f0f2edda1f34de632fb70fb53eb
Yann Massard <yamass@gmail.com>
2025-04-25 11:59:59 +0200

    replay: TimeGraph: prevent infinite loop (with very small numbers) while increasing dy in order to fit more ticks onto the y axle Yann Massard 2023-02-06 15:55

--- client/projector-timegraph/src/main/ts/AbstractGraph.ts
+++ client/projector-timegraph/src/main/ts/AbstractGraph.ts
@@ -129,9 +129,10 @@ export abstract class AbstractGraph<C extends DtoGraph = DtoGraph, D extends Dto
 					break;
 				} else if (numberOfSignificantDigits > this.config.maxTickDigits) {
 					const delta = maxY - minY;
-					console.debug(`Increasing dy from (${minY}:${maxY}) to (${minY - (delta / this.config.maxTickDigits)}:${maxY + (delta / this.config.maxTickDigits)})`)
-					minY = minY - (delta / this.config.maxTickDigits);
-					maxY = maxY + (delta / this.config.maxTickDigits);
+					let boundaryExpansion = Math.max(delta / 4, 1e-14);
+					console.debug(`Increasing dy from (${minY}:${maxY}) to (${minY - boundaryExpansion}:${maxY + boundaryExpansion})`)
+					minY = minY - boundaryExpansion;
+					maxY = maxY + boundaryExpansion;
 				} else {
 					break;
 				}
