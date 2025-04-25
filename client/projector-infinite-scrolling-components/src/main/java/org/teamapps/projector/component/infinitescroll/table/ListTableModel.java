43cde7f6c9da55258c91745fcd612b37799708b8
Yann Massard <yamass@gmail.com>
2025-04-25 20:00:49 +0200

    replay: ListTableModel: support sorting using property extractor Yann Massard 2023-04-04 12:28

--- client/projector-infinite-scrolling-components/src/main/java/org/teamapps/projector/component/infinitescroll/table/ListTableModel.java
+++ client/projector-infinite-scrolling-components/src/main/java/org/teamapps/projector/component/infinitescroll/table/ListTableModel.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,17 +20,22 @@
 package org.teamapps.projector.component.infinitescroll.table;
 
 import org.teamapps.projector.component.infinitescroll.infiniteitemview.RecordsAddedEvent;
+import org.teamapps.projector.dataextraction.BeanPropertyExtractor;
+import org.teamapps.projector.dataextraction.PropertyExtractor;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ListTableModel<RECORD> extends AbstractTableModel<RECORD> {
 
 	private List<RECORD> list = new ArrayList<>();
 	private Predicate<RECORD> filter = record -> true;
+	private PropertyExtractor<RECORD> sortingPropertyExtractor = new BeanPropertyExtractor<>();
 
 	public ListTableModel() {
 	}
@@ -74,15 +79,49 @@ public class ListTableModel<RECORD> extends AbstractTableModel<RECORD> {
 
 	@Override
 	public List<RECORD> getRecords(int startIndex, int length) {
-		if (filter == null) {
+		if (filter == null && sorting == null) {
 			return list.subList(startIndex, Math.min(list.size(), startIndex + length));
 		} else {
-			return list.stream()
-					.filter(filter)
+			Stream<RECORD> stream = list.stream();
+			if (filter != null) {
+				stream = stream.filter(filter);
+			}
+			if (sorting != null && sortingPropertyExtractor != null) {
+				stream = stream.sorted(valueExtractingComparator());
+			}
+			return stream
 					.skip(startIndex)
 					.limit(length)
 					.collect(Collectors.toList());
 		}
+	}
+
+	private Comparator<RECORD> valueExtractingComparator() {
+		Comparator<RECORD> comparator = (o1, o2) -> {
+			Object v1 = sortingPropertyExtractor.getValue(o1, sorting.getFieldName());
+			Object v2 = sortingPropertyExtractor.getValue(o2, sorting.getFieldName());
+			if (v1 == null && v2 == null) {
+				return 0;
+			} else if (v1 == null) {
+				return 1;
+			} else if (v2 == null) {
+				return -1;
+			} else { // both are not null
+				if (!(v1 instanceof Comparable) && !(v2 instanceof Comparable)) {
+					return 0;
+				} else if (!(v1 instanceof Comparable)) {
+					return 1;
+				} else if (!(v2 instanceof Comparable)) {
+					return -1;
+				} else {
+					return ((Comparable) v1).compareTo(v2);
+				}
+			}
+		};
+		if (sorting.getSortDirection() == SortDirection.DESC) {
+			comparator = comparator.reversed();
+		}
+		return comparator;
 	}
 
 	public List<RECORD> getAllRecords() {
@@ -96,5 +135,13 @@ public class ListTableModel<RECORD> extends AbstractTableModel<RECORD> {
 	public void setFilter(Predicate<RECORD> filter) {
 		this.filter = filter;
 		onAllDataChanged.fire(null);
+	}
+
+	public PropertyExtractor<RECORD> getSortingPropertyExtractor() {
+		return sortingPropertyExtractor;
+	}
+
+	public void setSortingPropertyExtractor(PropertyExtractor<RECORD> sortingPropertyExtractor) {
+		this.sortingPropertyExtractor = sortingPropertyExtractor;
 	}
 }
