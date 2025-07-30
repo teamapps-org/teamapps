06b62066e254f2b7ab86186e99926a45bd1d3031
Yann Massard <yamass@gmail.com>
2025-07-30 13:58:55 +0200

    migrate to new publishing plugin

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export JDK_JAVA_OPTIONS='--add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED --add-opens java.base/java.text=ALL-UNNAMED --add-opens java.desktop/java.awt.font=ALL-UNNAMED'
-mvn -e -P javadoc-jar,sources-jar,license-handling,release release:prepare release:perform && cd target/checkout && mvn nexus-staging:release
+mvn -e -P javadoc-jar,sources-jar,license-handling,release release:prepare release:perform && cd target/checkout && mvn central-publishing:release
