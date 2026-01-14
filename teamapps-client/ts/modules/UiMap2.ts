9e682c44caaf9debf97de15105ac6c985c3bb258
Yann Massard <yamass@gmail.com>
2026-01-14 16:41:50 +0100

    Prevent map error from blocking the whole UI

--- teamapps-client/ts/modules/UiMap2.ts
+++ teamapps-client/ts/modules/UiMap2.ts
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -112,6 +112,10 @@ export class UiMap2 extends AbstractUiComponent<UiMap2Config> implements UiMap2E
 		}
 		this.map.on('load', () => {
 			this.onResize();
+			this.deferredExecutor.ready = true;
+		});
+		this.map.on('error', (errorEvent) => {
+			console.log("Error in map!", errorEvent)
 			this.deferredExecutor.ready = true;
 		});
 
