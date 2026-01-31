0c3ff81c7419ff01d33d21c03e35eb20a4c2153f
Benjamin Jesuiter <bjesuiter@gmail.com>
2026-01-31 17:39:10 +0100

    UiPdfViewer: add test harness component

---
+++ teamapps-ux/src/main/java/org/teamapps/ux/component/pdfviewer/UiPdfViwerTestHarness.java
@@ -0,0 +1,77 @@
+/*-
+ * ========================LICENSE_START=================================
+ * TeamApps
+ * ---
+ * Copyright (C) 2014 - 2025 TeamApps.org
+ * ---
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =========================LICENSE_END==================================
+ */
+package org.teamapps.ux.component.pdfviewer;
+
+import org.teamapps.dto.UiPdfViewMode;
+import org.teamapps.dto.UiPdfZoomMode;
+import org.teamapps.ux.component.panel.Panel;
+import org.teamapps.ux.component.toolbar.Toolbar;
+import org.teamapps.ux.component.toolbar.ToolbarButton;
+import org.teamapps.ux.component.toolbar.ToolbarButtonGroup;
+import org.teamapps.ux.resource.ClassPathResource;
+
+public class UiPdfViwerTestHarness extends Panel {
+
+	public UiPdfViwerTestHarness() {
+		super(null, "PDF Viewer");
+
+		PdfViewer pdfViewer = new PdfViewer();
+		pdfViewer.setPadding(10);
+		pdfViewer.setPageSpacing(8);
+		pdfViewer.setShowDevTools(true);
+		pdfViewer.setZoomMode(UiPdfZoomMode.TO_HEIGHT);
+		pdfViewer.setBackgroundColor("oklch(0.74 0.1 218.65)");
+		pdfViewer.setBorderColor("#ff0000");
+
+		Toolbar toolbar = new Toolbar();
+		ToolbarButtonGroup buttonGroup = toolbar.addButtonGroup(new ToolbarButtonGroup());
+
+		ToolbarButton showPdfButton = buttonGroup.addButton(ToolbarButton.create(null, "Show PDF", "Show the PDF document"));
+		ToolbarButton continuousModeButton = buttonGroup.addButton(ToolbarButton.create(null, "Continuous Mode", "Switch to continuous scrolling mode"));
+		ToolbarButton singlePageModeButton = buttonGroup.addButton(ToolbarButton.create(null, "Single Page Mode", "Switch to single page mode"));
+
+		String testPdfLink = getSessionContext().createResourceLink(new ClassPathResource("test.pdf", "application/pdf"));
+
+		showPdfButton.onClick.addListener(() -> {
+			pdfViewer.setUrl(testPdfLink);
+		});
+
+		continuousModeButton.onClick.addListener(() -> {
+			pdfViewer.setViewMode(UiPdfViewMode.CONTINUOUS);
+			System.out.println("Switched to CONTINUOUS mode");
+		});
+
+		singlePageModeButton.onClick.addListener(() -> {
+			pdfViewer.setViewMode(UiPdfViewMode.SINGLE_PAGE);
+			System.out.println("Switched to SINGLE_PAGE mode");
+		});
+
+		pdfViewer.onPdfInitialized.addListener((initEvent) -> {
+			System.out.println("PDF viewer rendered, page number: " + initEvent.getNumberOfPages());
+		});
+
+		pdfViewer.onZoomFactorAutoChanged.addListener((zoomFactorChangeEvent) -> {
+			System.out.println("Zoom factor changed: " + zoomFactorChangeEvent.getZoomFactor());
+		});
+
+		setToolbar(toolbar);
+		setContent(pdfViewer);
+	}
+}
